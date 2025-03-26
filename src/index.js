d88701020e13c500c3c5afb8fc12dd82fc1bf5bd
dafacen <cen.y@northeastern.edu>
2025-03-25 20:57:33 -0700

    Fix routing issues for deployment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles/global.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
